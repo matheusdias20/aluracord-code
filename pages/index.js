7490c711f1ee8d4e7da8c066b773029f3587545d
Matheus Vieira <mvdias20@gmail.com>
2022-01-27 19:49:50 -0300

    useState and useRouter

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+
 
 function Title(props) {
 
@@ -51,11 +26,12 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-    const username = 'matheusdias20';
-  
+    // const username = 'matheusdias20';
+    const [username, setUsername] = React.useState('matheusdias20');
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -81,6 +57,10 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (event) {
+                event.preventDefault();
+                roteamento.push('/chat')
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -92,6 +72,12 @@ export default function PaginaInicial() {
               </Text>
   
               <TextField
+                value={username}
+                onChange={function handler (event) {
+                  const valor = event.target.value
+                  setUsername(valor)
+                }}
+                placeholder='Digite seu usuário do github'
                 fullWidth
                 textFieldColors={{
                   neutral: {
